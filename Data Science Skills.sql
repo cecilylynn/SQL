e3284aa5c0cc531de90a6ccbf5c99e0f8f08a65e
cecilylynn <113400825+cecilylynn@users.noreply.github.com>
2024-10-11 15:36:51 -0400

    Add files via upload

---
+++ Data Science Skills.sql
@@ -0,0 +1,16 @@
+--Given a table of candidates and their skills, you're tasked with finding the candidates best suited for an open Data Science job. You want to find candidates who are proficient in Python, Tableau, and PostgreSQL.
+
+--Write a query to list the candidates who possess all of the required skills for the job. Sort the output by candidate ID in ascending order.
+
+SELECT candidate_id
+FROM
+  (
+  SELECT candidate_id
+    , skill
+  FROM candidates
+  WHERE skill IN ('Python', 'Tableau', 'PostgreSQL')
+  ) as candidates2
+GROUP BY 1
+HAVING COUNT(skill) =3
+ORDER BY candidate_id ASC
+;
