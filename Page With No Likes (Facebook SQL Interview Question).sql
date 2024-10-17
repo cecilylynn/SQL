7507df1686f7599f310abf5691c117b364f2e569
cecilylynn <113400825+cecilylynn@users.noreply.github.com>
2024-10-17 15:12:35 -0400

    Added a second method

--- Page With No Likes (Facebook SQL Interview Question).sql
+++ Page With No Likes (Facebook SQL Interview Question).sql
@@ -6,10 +6,21 @@
 -- Write a query to return the IDs of the Facebook pages that have zero likes. 
 -- The output should be sorted in ascending order based on the page IDs.
 
+
+-- method 1: JOIN
 SELECT p.page_id 
 FROM pages as p
   LEFT JOIN page_likes as l
   ON p.page_id = l.page_id
 WHERE user_id IS NULL
 ORDER BY page_id ASC
+;
+
+
+-- method 2: EXCEPT
+SELECT page_id
+FROM pages
+EXCEPT 
+SELECT page_id
+FROM page_likes
 ;
